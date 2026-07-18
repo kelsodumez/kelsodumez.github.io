f3e56ffb83ee88fc4e2a156f0b4a8d2c6ea3cbed
kelsodumez <kelsodumez@gmail.com>
2026-07-19 11:06:47 +1200

    Working on my column-grid layout implementation.

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
     return (
-            <main className="flex flex-1 w-full max-w-8/12 flex-col items-center justify-between py-32 px-8 bg-(--background) sm:items-start">
+            // <main> className="flex flex-1 w-full max-w-8/12 flex-col items-center justify-between py-32 px-8 bg-(--background) sm:items-start">
+        <main>
                 <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
                     <h1 className="max-w-12/12 text-8xl font-bold leading-22 tracking-tight text-black">
                         About Me
@@ -25,31 +26,31 @@ export default function Home() {
                         </a>{" "}
                     </p>
                 </div>
-                <div className="flex flex-col gap-8 text-base font-medium sm:flex-row">
-                    <a
-                        className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] md:w-39.5"
-                        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <Image
-                            className="dark:invert"
-                            src="/vercel.svg"
-                            alt="Vercel logomark"
-                            width={16}
-                            height={16}
-                        />
-                        Get in Touch
-                    </a>
-                    <a
-                        className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/8 px-5 transition-colors hover:border-transparent hover:bg-black/4  md:w-39.5"
-                        href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        See my work
-                    </a>
-                </div>
+                {/*<div className="flex flex-col gap-8 text-base font-medium sm:flex-row">*/}
+                {/*    <a*/}
+                {/*        className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] md:w-39.5"*/}
+                {/*        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"*/}
+                {/*        target="_blank"*/}
+                {/*        rel="noopener noreferrer"*/}
+                {/*    >*/}
+                {/*        <Image*/}
+                {/*            className="dark:invert"*/}
+                {/*            src="/vercel.svg"*/}
+                {/*            alt="Vercel logomark"*/}
+                {/*            width={16}*/}
+                {/*            height={16}*/}
+                {/*        />*/}
+                {/*        Get in Touch*/}
+                {/*    </a>*/}
+                {/*    <a*/}
+                {/*        className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/8 px-5 transition-colors hover:border-transparent hover:bg-black/4  md:w-39.5"*/}
+                {/*        href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"*/}
+                {/*        target="_blank"*/}
+                {/*        rel="noopener noreferrer"*/}
+                {/*    >*/}
+                {/*        See my work*/}
+                {/*    </a>*/}
+                {/*</div>*/}
             </main>
     );
 }
